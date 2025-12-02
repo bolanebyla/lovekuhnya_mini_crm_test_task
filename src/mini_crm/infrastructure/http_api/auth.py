4697d0bd04ea79082acae36792d36574488bf831
Алексей Лесков <alexleskov2000@gmail.com>
2025-12-02 17:19:29 +0800

    X-Organization-Id header

--- src/mini_crm/infrastructure/http_api/auth.py
+++ src/mini_crm/infrastructure/http_api/auth.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from dishka import FromDishka
 from dishka.integrations.fastapi import inject
-from fastapi import Depends
+from fastapi import Depends, Header
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 
@@ -17,15 +17,18 @@ class CurrentUser(BaseModel):
     """Текущий пользователь"""
 
     user_id: int
+    organization_id: int
 
 
 @inject
 def get_current_user(
     access_token: Annotated[str, Depends(oauth2_scheme)],
+    organization_id: Annotated[int, Header(alias="X-Organization-Id")],
     jwt_manager: FromDishka[JwtManager],
 ) -> CurrentUser:
     token_claims = jwt_manager.verify_access_token(token=access_token)
     current_client = CurrentUser(
         user_id=int(token_claims.sub),
+        organization_id=organization_id,
     )
     return current_client
