d086786b62cdf0de57b6b0a1ab8b1a99611ebdb0
Алексей Лесков <alexleskov2000@gmail.com>
2025-12-04 11:43:16 +0800

    Activity mapping

--- src/mini_crm/application/activities/__init__.py
+++
@@ -1,3 +0,0 @@
-from .entities import Activity
-from .enums import ActivityTypes
-from .interfaces import ActivitiesRepo
--- src/mini_crm/infrastructure/database/mapping.py
+++ src/mini_crm/infrastructure/database/mapping.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import registry
 
+from mini_crm.application.activities.entities import Activity
 from mini_crm.application.contacts.entities import Contact
 from mini_crm.application.deals.entities import Deal
 from mini_crm.application.tasks.entities import Task
 from mini_crm.infrastructure.database.tables import (
+    activities_table,
     contacts_table,
     deals_table,
     tasks_table,
@@ -14,3 +16,4 @@ mapper = registry()
 mapper.map_imperatively(Contact, contacts_table)
 mapper.map_imperatively(Deal, deals_table)
 mapper.map_imperatively(Task, tasks_table)
+mapper.map_imperatively(Activity, activities_table)
